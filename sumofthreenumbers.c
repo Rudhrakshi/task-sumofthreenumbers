91d4dc4d7f84797b777211c203bae60e437296c5
Rudhrakshi <67533685+Rudhrakshi@users.noreply.github.com>
2020-06-29 00:50:29 +0530

    Add files via upload

---
+++ sumofthreenumbers.c
@@ -0,0 +1,10 @@
+// task:getting multiple input from user 28 june 2020
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter three integers a,b and c:");
+	scanf("%d%d%d",&a,&b,&c);
+	int d=a+b+c;
+    printf("sum of %d+%d+%d=%d",a,b,c,d);
+}
